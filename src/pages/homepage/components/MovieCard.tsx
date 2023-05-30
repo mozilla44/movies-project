0f849c65b199628f351f7e1468a5488f33f1b03c
gabykur <gabrielekuraite@gmail.com>
2023-05-30 10:15:52 +0200

    App.tsx is now displaying all the popular movies with MovieList and the intermidiate component MovieCard"

--- src/pages/homepage/components/MovieCard.tsx
+++ src/pages/homepage/components/MovieCard.tsx
@@ -1,45 +1,25 @@
 import React from "react";
 import "./MovieCard.css";
+import { MovieType } from "../../../models/Movie";
 
-export const MovieCard = () => {
-  const imgUrl =
-    "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/kTh1s6I6yUyk2OGiRoGkDTYTS6K.jpg";
-  const title = "Top Gun: Maverick";
-  const score = 8.2;
+type MovieCardProps = {
+ movie: MovieType;
+};
+
+export const MovieCard = ({movie}:MovieCardProps) => {
+  // console.log("Image URL:", movie.poster_path);
+  // console.log("Score:", movie.popularity);
+
+  const imageUrl = `https://image.tmdb.org/t/p/w200${movie.poster_path}`;
 
   return (
     <div className="movie-container">
       <div className="movie-card">
-        <img src={imgUrl} alt={title} className="image" />
-        <h3 className="title">{title}</h3>
-        <div className="score"> Score:{score}</div>
-      </div>
-      <div className="movie-card">
-        <img src={imgUrl} alt={title} className="image" />
-        <h3 className="title">{title}</h3>
-        <div className="score"> Score:{score}</div>
-      </div>
-      <div className="movie-card">
-        <img src={imgUrl} alt={title} className="image" />
-        <h3 className="title">{title}</h3>
-        <div className="score"> Score:{score}</div>
-      </div>
-      <div className="movie-card">
-        <img src={imgUrl} alt={title} className="image" />
-        <h3 className="title">{title}</h3>
-        <div className="score"> Score: {score}</div>
-      </div>
-      <div className="movie-card">
-        <img src={imgUrl} alt={title} className="image" />
-        <h3 className="title">{title}</h3>
-        <div className="score"> Score: {score}</div>
-      </div>
-      <div className="movie-card">
-        <img src={imgUrl} alt={title} className="image" />
-        <h3 className="title">{title}</h3>
-        <div className="score"> Score: {score}</div>
+        <img src={imageUrl} alt={movie.title} className="image" />
+        <h3 className="title">{movie.title}</h3>
+        <div className="score"> Score:{movie.popularity}</div>
+        <div className="id"> ID: {movie.id}</div>
       </div>
     </div>
   );
 };
- 
