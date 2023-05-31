e5f12fcbb4bd4d52947e58eba814737f65872d11
antoine <antoine.frisco@gmail.com>
2023-05-31 12:22:16 +0200

    moved     etchMoviesByCategory() in movieAPI

--- src/api/categoryAPI.ts
+++ src/api/categoryAPI.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { Category } from "../models/Categories";
+import { MovieType } from "../models/Movie";
 
 const API_KEY = import.meta.env.VITE_API_KEY as string;
 
@@ -15,3 +16,17 @@ export const getCategories = async (): Promise<Category[]> => {
   }
 };
 
+
+export const fetchMoviesByCategory = async (categoryId: number) : Promise<MovieType[]> => {
+  const API_KEY = import.meta.env.VITE_API_KEY as string;
+      try {
+          const URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=${categoryId}`;
+          const response = await axios.get<{ results: MovieType[] }>(URL);
+          console.log(response.data.results);
+          console.log("coucou")
+          return response.data.results;
+        } catch (error) {
+          console.log(error);
+          throw new Error("Failed to fetch categories"); 
+        }
+    };
--- src/pages/homepage/components/CategoryBtn.tsx
+++ src/pages/homepage/components/CategoryBtn.tsx
@@ -1,26 +1,14 @@
 import { Category } from "../../../models/Categories";
 import { MovieType } from "../../../models/Movie";
-import  axios  from "axios";
+import { fetchMoviesByCategory } from "../../../api/categoryAPI";
 
 type CategoryBtnProps = {
     category: Category
-    movie:MovieType
+    movie?:MovieType
 }
 
 
 export const CategoryBtn = ({category}: CategoryBtnProps) => {
-     const fetchMoviesByCategory = async (categoryId: number) : Promise<MovieType[]> => {
-    const API_KEY = import.meta.env.VITE_API_KEY as string;
-        try {
-            const URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=${categoryId}`;
-            const response = await axios.get<{ results: MovieType[] }>(URL);
-            console.log(response.data.results);
-            return response.data.results;
-          } catch (error) {
-            console.log(error);
-            throw new Error("Failed to fetch categories"); 
-          }
-      };
     return (
         <button className="category_btn" onClick={()=>fetchMoviesByCategory(category.id)}>
             {category.name}
@@ -29,19 +17,3 @@ export const CategoryBtn = ({category}: CategoryBtnProps) => {
 }
 
 
-/* export const CategoryBtn = ({category}: CategoryBtnProps) => {
-    const fetchMoviesByCategory = async (categoryId: number) : Promise<Category[]> => {
-       console.log(category.id)
-       const API_KEY = import.meta.env.VITE_API_KEY as string;
-       const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=${categoryId}`;
-       const response = await fetch(url);
-       const data = await response.json();
-       return data.results;
-       
-     };
-   return (
-       <button className="category_btn" onClick={()=>fetchMoviesByCategory(27)}>
-           {category.name}
-       </button>
-   );
-} */
