53b02dabd67597feb32c0e899c17acf057afadef
Antoine <antoine.frisco@gmail.com>
2024-02-06 15:54:00 +0100

    fixed menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { Category } from "../models/Categories";
 import { getCategories } from "../api/categoryAPI";
@@ -31,20 +31,14 @@ export const Header = () => {
   }, []);
   return (
     <header className="header">
-      <Link to="/" className="logo_link">
+      <NavLink  to="/" className="logo_link"  >
         <img src={logo} className="logo" alt="logo"></img>
-      </Link>
+      </NavLink>
       <Link to=""><div className="app_title">Popcorn Watch</div></Link>
       
-      
-
       <nav className="topnav">
-        <Link to="/" className="header_links">
-          Trending
-        </Link>
-        <Link to="/upcoming" className="header_links">
-          Upcoming
-        </Link>
+        <NavLink to="/" className={({ isActive }) => (isActive ? 'active_link' : "header_links")}>Trending</NavLink>
+        <NavLink to="/upcoming" className={({ isActive }) => (isActive ? 'active_link' : "header_links")}>Upcoming</NavLink>
       </nav>
     </header>
   );
